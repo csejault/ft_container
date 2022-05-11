41be25547e73db70d3d0c195e06e9c4d61cb1973
Clement SEJAULT <csejault@e3r10p14.clusters.42paris.fr>
2022-05-11 17:00:00 +0200

    fix: fix iterators

--- include/iterator.hpp
+++ include/iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: csejault <csejault@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 12:22:05 by csejault          #+#    #+#             */
-/*   Updated: 2022/05/11 16:49:41 by csejault         ###   ########.fr       */
+/*   Updated: 2022/05/11 16:59:43 by csejault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef ITERATOR_HPP
@@ -75,11 +75,6 @@ namespace ft {
 				explicit reverse_iterator( iterator_type x ) : _ptr(x) { }
 				template< class U >
 					reverse_iterator( const reverse_iterator<U>& other ) : _ptr(other.base()) { }
-				template< class U >
-					reverse_iterator& operator=( const reverse_iterator<U>& other )
-					{
-						_ptr = other.base();
-					}
 
 				~reverse_iterator( void ) {};
 
@@ -171,118 +166,5 @@ namespace ft {
 			return ( !(lhs < rhs) );
 		}
 
-	//	template <class T>
-	//		class	random_access_iterator
-	//		{
-	//			public:
-	//
-	//				typedef	T								value_type;
-	//				typedef ft::random_access_iterator_tag	iterator_category;
-	//				typedef	ptrdiff_t						difference_type;
-	//				typedef	T*								pointer;
-	//				typedef T&								reference; 
-	//				typedef	random_access_iterator			class_name;
-	//
-	//
-	//				////////////////////////////////////////////////////////////////////////////////////
-	//				//                                                                                //
-	//				// Is default-constructible, copy-constructible, copy-assignable and destructible //
-	//				//                                                                                //
-	//				////////////////////////////////////////////////////////////////////////////////////
-	//
-	//				~random_access_iterator( void );
-	//				random_access_iterator( void ) : _ptr(NULL) {}
-	//				random_access_iterator( random_access_iterator<T> &to_cpy ) : _ptr(to_cpy.get_ptr()) {}
-	//				random_access_iterator & operator=( const random_access_iterator<T>& asign ) { if (&asign != this)_ptr = asign.get_ptr(); }
-	//
-	//
-	//				/////////////////////////////////////////////////////////////////////////////
-	//				//                                                                         //
-	//				// Can be compared for equivalence using the equality/inequality operators //
-	//				//                                                                         //
-	//				/////////////////////////////////////////////////////////////////////////////
-	//
-	//				bool	operator==( const random_access_iterator<T> &rhs ) const { return _ptr == rhs._ptr; }
-	//				bool	operator!=( const random_access_iterator<T> &rhs ) const { return !(*this == rhs); }
-	//
-	//
-	//				///////////////////////////////////////////////////////////////////////
-	//				//                                                                   //
-	//				// Can be dereferenced as an rvalue (if in a dereferenceable state). //
-	//				//                                                                   //
-	//				///////////////////////////////////////////////////////////////////////
-	//
-	//				reference	operator*( void ) const { return *_ptr; }
-	//				pointer		operator->( void ) const { return _ptr; }
-	//
-	//
-	//				/////////////////////////////////////////////////////////
-	//				//                                                     //
-	//				// Can be incremented (if in a dereferenceable state). //
-	//				//                                                     //
-	//				/////////////////////////////////////////////////////////
-	//
-	//				class_name &	operator++( void ) { ++_ptr; return *this; }
-	//				class_name 		operator++( int ) { class_name to_ret = *this; ++*this; return to_ret; }
-	//
-	//
-	//				///////////////////////////////////////////////////////////////////////////
-	//				//                                                                       //
-	//				// Can be decremented (if a dereferenceable iterator value precedes it). //
-	//				//                                                                       //
-	//				///////////////////////////////////////////////////////////////////////////
-	//
-	//				class_name &	operator--( void ) { --_ptr; return *this; }
-	//				class_name 		operator--( int ) { class_name to_ret = *this; --*this; return to_ret; }
-	//
-	//
-	//				//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	//				//                                                                                                                              //
-	//				// Supports the arithmetic operators + and - between an iterator and an integer value, or subtracting an iterator from another. //
-	//				//                                                                                                                              //
-	//				//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	//
-	//				//???n + a
-	//				class_name &	operator+( const difference_type & nb ) { return static_cast<class_name>(_ptr + nb); }
-	//
-	//				class_name &	operator-( const difference_type & nb ) { return static_cast<class_name>(_ptr - nb); }
-	//				difference_type &	operator-( const class_name & rai ) { return _ptr - rai._ptr; }
-	//
-	//				pointer get_ptr( void ) const { return _ptr; }
-	//
-	//
-	//				/////////////////////////////////////////////////////////////////////////////
-	//				//                                                                         //
-	//				// Can be compared with inequality relational operators (<, >, <= and >=). //
-	//				//                                                                         //
-	//				/////////////////////////////////////////////////////////////////////////////
-	//
-	//				bool	operator<( const class_name & rhs ) const { return _ptr < rhs._ptr; }
-	//				bool	operator>( const class_name & rhs ) const { return _ptr > rhs._ptr; }
-	//				bool	operator<=( const class_name & rhs ) const { return !(*this > rhs); }
-	//				bool	operator>=( const class_name & rhs ) const { return !(*this < rhs); }
-	//
-	//
-	//				///////////////////////////////////////////////////////
-	//				//                                                   //
-	//				// Supports compound assignment operations += and -= //
-	//				//                                                   //
-	//				///////////////////////////////////////////////////////
-	//
-	//				class_name &	operator+=( const difference_type & nb ) { _ptr += nb; return *this;}
-	//				class_name &	operator-=( const difference_type & nb ) { _ptr -= nb; return *this;}
-	//
-	//
-	//				///////////////////////////////////////////////////
-	//				//                                               //
-	//				// Supports the offset dereference operator ([]) //
-	//				//                                               //
-	//				///////////////////////////////////////////////////
-	//
-	//				reference			operator[](const difference_type & nb) { return *(_ptr + nb); }
-	//
-	//			private:
-	//				pointer _ptr;
-	//		};
 }
 #endif
